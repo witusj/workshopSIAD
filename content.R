5956e887fe7f8beaf8a9b4e47cde3e33b330166f
witusj <wfth69@gmail.com>
2016-08-09 00:12:56 +0200

    Minor

--- content.R
+++ content.R
@@ -1,9 +1,14 @@
 library(googleVis)
 op <- options(gvis.plot.tag='chart')
 
+adres = "HAN, Kapittelweg 33, 6525 EN Nijmegen"
+extra = "Lokaal C106"
+directions = "<a href='https://goo.gl/maps/PA9xeFzKYNp'>Get directions</a>"
+
 locaties <- data.frame(adres = "HAN, Kapittelweg 33, 6525 EN Nijmegen",
-                       tooltip = "Lokaal C106"
-)
+                       tooltip= paste0(extra, "<br>", directions)
+                       )
+
 M1 <- gvisMap(locaties, "adres" , "tooltip",
               options=list(showTip=TRUE,
                            mapType='normal',
@@ -12,3 +17,4 @@ M1 <- gvisMap(locaties, "adres" , "tooltip",
                            height=300
               )
 )
+plot(M1)
